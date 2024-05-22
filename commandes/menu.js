7d3cb5e3dae5e5b4dbc8864ef31912a567c8285e
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-05-22 22:57:35 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -32,7 +32,7 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
-╭────✧${s.BOT}✧────◆
+╭────✧{𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿}✧────◆
 │   *Préfix* : ${s.PREFIXE}
 │   *Owner* : ${s.OWNER_NAME}
 │   *Mode* : ${mode}
@@ -41,8 +41,8 @@ const date = moment().format('DD/MM/YYYY');
 │   *Hour* : ${temps}
 │   *Mémoire* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
 │   *Plateforme* : ${os.platform()}
-│   *Développer* : Djalega++ 
-│  & M๏𝓷keℽ D Lบffy
+│   *Développer* : Lazack_28
+│ 
 ╰─────✧WA-BOT✧─────◆ \n\n`;
     
 let menuMsg = `
@@ -67,7 +67,7 @@ let menuMsg = `
 *»»————— ★ —————««*
 "To use a command, insert ${prefixe} followed by the command_name."
  
-    Powered by Zokou-MD
+    LAZACK-MD
                                                 
 *»»————— ★ —————««*
 `;
