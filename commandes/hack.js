345ebcb6daee6db082dd4b03ba9dd3bcb409b8ea
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-05-24 12:15:54 +0300

    Create hack.js

---
+++ commandes/hack.js
@@ -0,0 +1,92 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+zokou({ nomCom: 'hack',
+    desc: 'hacking someones Whatsapp',
+    Categorie: 'prank',
+    reaction: '🦠', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    
+citel.reply("```Lazack-md Injecting malware```")
+
+await sleep(1000)
+
+citel.reply("```hacking into device \n 0%```")
+
+await sleep(1000)
+
+citel.reply("```transfering photos \n █ 10%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ 20%```")
+
+await sleep(1000)
+
+citel.reply("```transfering videos \n █ █ █ 30%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ 40%```")
+
+await sleep(1000)
+
+citel.reply("```transfering audio \n █ █ █ █ █ 50%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ 60%```")
+
+await sleep(1000)
+
+citel.reply("```transfering hidden files \n █ █ █ █ █ █ █ 70%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ █ █ 80%```")
+
+await sleep(1000)
+
+citel.reply("```transfering whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ █ █ █ █ 100%```")
+
+await sleep(1000)
+
+citel.reply("```System hyjacking on process.. \n Conecting to Server ```")
+
+await sleep(1000)
+
+citel.reply("```Divice successfully connected... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```")
+
+await sleep(1000)
+
+citel.reply("``` HACKING COMPLETED ```")
+
+await sleep(1000)
+
+citel.reply("``` SENDING PHONE DOCUMENTS...```")
+
+await sleep(1000)
+
+citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
+
+await sleep(1000)
+
+            return citel.reply('*ALL FILES TRANSFERRED*');
+
+        }
+)
