059fd807ac097b67d00a1533c3fc8c69ad01e0c0
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-05-26 11:44:27 +0300

    Update parole.js

--- lazack/parole.js
+++ lazack/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------ZK-lyrics-finder--------
+    let msg = '━━━━❀˖°❀˖°❀˖°*Lazack-lyrics*━━━━━❀˖°❀˖°❀˖°
 
 * *Artist :* ${lyrics.artist}
 
