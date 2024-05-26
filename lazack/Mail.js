2dc2efd32905f6d2110a4ff540c8ae476fa0d1b4
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-05-26 23:53:37 +0300

    Update Mail.js

--- lazack/Mail.js
+++ lazack/Mail.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*LAZACK-MD* Temporary emails,Powered by *France King* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *Lazackmd.*\n\n";
+    let z = '*LAZACK-MD* Temporary emails \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *Lazackmd.*\n\n";
     let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/4415fe12b1203e4caeecf.jpg';
