204e0f69c76ff28dbddc4b52686518754d013d79
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-08 21:02:35 +0300

    Update Mail.js

--- lazack/Mail.js
+++ lazack/Mail.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '*LAZACK-MD* Temporary emails \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *Lazackmd.*\n\n";
-    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
+    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com \n\n 5 https://www.mailslurp.com/tools/fake-email-generator/';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/4415fe12b1203e4caeecf.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
