d6fc518957aa31c24364abb32ba42b95bf4a80ba
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-12 23:55:27 +0300

    Add files via upload

---
+++ assets/database/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Notes: require("./notes"),
+  Plugins: require("./plugins"),
+  Filters: require("./filters"),
+  Greetings: require("./greetings"),
+  PausedChats:require("./PausedChat"),
+  WarnDB:require('./warn')
+};
