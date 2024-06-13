067791ee0cac725427efeeab2088ee3a27b5ba67
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-13 11:26:56 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -19,7 +19,7 @@ Secktor.cmd({
     },
     async(Void, citel) => {
         var inital = new Date().getTime();
-        const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
+        const { key } = await Void.sendMessage(citel.chat, {text: '```𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿 Ping!!!```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
        return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
