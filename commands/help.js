1d2600e384b652be21ec35626cd3326071eb58e2
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-14 01:28:51 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -93,13 +93,13 @@ Secktor.cmd({
         async(Void, citel) => {
             const { commands } = require('../lib');
             let str = `
-┏━━━━━•❃〘 ` + fancytext(Config.ownername.split(' ')[0], 58) + ` 〙❃•━━━━━┓`
+┏━━━━━•❃〘LAZACK-MD〙❃•━━━━━┓`
             str += `
 ┃ ⛥┏━━━━━•❃°•°•━━━━━•❃°•°•      
 •͙͙✧⃝•͙ User: ${citel.pushName}
 •͙͙✧⃝•͙ Theme: ${tlang().title}
 •͙͙✧⃝•͙│ Prefix: ${prefix}
-•͙͙✧⃝•͙ Owner: ${Config.ownername}
+•͙͙✧⃝•͙ Owner: lazack
 •͙͙✧⃝•͙ Commands: ${commands.length}
 •͙͙✧⃝•͙ Uptime: ${runtime(process.uptime())}
 •͙͙✧⃝•͙ Mem: ${formatp(os.totalmem() - os.freemem())}/${formatp(os.totalmem())}
