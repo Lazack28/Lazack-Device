912053c6c9c31d7aab56fac21cbd61f8e53cb530
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-13 07:16:23 +0300

    Delete assets directory

--- assets/database/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  Notes: require("./notes"),
-  Plugins: require("./plugins"),
-  Filters: require("./filters"),
-  Greetings: require("./greetings"),
-  PausedChats:require("./PausedChat"),
-  WarnDB:require('./warn')
-};
