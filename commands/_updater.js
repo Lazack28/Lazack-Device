2954d17f7f35b73889e5415499676a79ef6d88fa
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-16 18:21:18 +0300

    Update _updater.js

--- commands/_updater.js
+++ commands/_updater.js
@@ -1,14 +1,3 @@
-/**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : XLICON-MD
- * @author : salmanytofficial <https://github.com/salmanytofficial/XLICON-MD>
- * @description : XLICON,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
-
 const DB = require('../lib/scraper')
 const { execSync } = require('child_process')
 const { tlang, Config, prefix,cmd } = require('../lib')
