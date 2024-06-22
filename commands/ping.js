21e7b444e9afc7751cc8097e1d6be4aa3ddbe550
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-22 20:21:15 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -17,11 +17,44 @@ Secktor.cmd({
         category: "general",
         filename: __filename,
     },
-    async(Void, citel) => {
-        var inital = new Date().getTime();
-        const { key } = await Void.sendMessage(citel.chat, {text: '```𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿 Ping!!!```'});
-        var final = new Date().getTime();
-       // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
+    const ping = async (m, sock) => {
+  const prefix = /^[\\/!#.]/gi.test(m.body) ? m.body.match(/^[\\/!#.]/gi)[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
+  if (cmd === "ping") {
+
+    const startTime = new Date();
+    const { key } = await sock.sendMessage(m.from, { text: '*_Pinging..._*' }, { quoted: m });
+    await m.React('🚀');
+
+    const text = `*_🔥⃝вσт ѕρєє∂: ${new Date() - startTime} ms_*`;
+    await typeWriterEffect(m, sock, key, text);
+
+    await m.React('⚡');
+  }
+}
+
+const typeWriterEffect = async (m, sock, key, message) => {
+  const typingSpeed = 300;
+  const words = message.split(' ');
+  let i = 0;
+
+  const typewriterInterval = setInterval(() => {
+    if (i < words.length) {
+      const typedText = words.slice(0, i + 1).join(' ');
+      sock.relayMessage(m.from, {
+        protocolMessage: {
+          key: key,
+          type: 14,
+          editedMessage: {
+            conversation: typedText,
+          },
+        },
+      }, {});
+      i++;
+    } else {
+      clearInterval(typewriterInterval); 
     }
-);
+  }, typingSpeed);
+}
+
+export default ping;
