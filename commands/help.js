f91ebe2eeba381b329d47d126c018f7a6de002b5
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-06-24 10:16:48 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -60,12 +60,12 @@ Secktor.cmd({
                 for (const category in cmds) 
                     {
                    str += `––〘 *${tiny(category)}* 〙–––►\n` ;
-                   if(text.toLowerCase() == category.toLowerCase()){ str = ` *${tiny(category)}* ✤\n` ;      
+                   if(text.toLowerCase() == category.toLowerCase()){ str = ` *${tiny(category)}* \n` ;      
                         for (const plugins of cmds[category]) { str += `►${fancytext(plugins,1)}\n` ; }
                         str += `–––––––––►\n`  ;
                         break ;
                    }
-                   else { for (const plugins of cmds[category]) { str += `┆➪${fancytext(plugins,1)}\n` ; }
+                   else { for (const plugins of cmds[category]) { str += `➪${fancytext(plugins,1)}\n` ; }
                          str += `–––––––––►\n`  ; 
                    }
  
