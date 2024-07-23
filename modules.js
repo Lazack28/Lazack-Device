45cd7df6d8f3f14d4e8a8ad72dd2f9dafb2d0a89
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-07-23 07:48:28 +0300

    Update and rename shizo_modules.js to modules.js

---
+++ modules.js
@@ -0,0 +1,8 @@
+//Shizo_modules importing system 
+import GIFBufferToVideoBuffer from './modules/giftobuffer.js'
+import displayLoadingScreen from './modules/loading.js'
+
+
+//Shizo_modules quick access system
+global.GIFBufferToVideoBuffer = GIFBufferToVideoBuffer
+global.displayLoadingScreen = displayLoadingScreen
