94af81056886e2b3e963f06e8bbff2bfd1bf8857
Canyonjunior <dalveer2356@gmail.com>
2024-08-01 10:37:20 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 
 //OwnerShip
 global.owner = [
-  [process.env.OWNER_NUMBER || '255734980103', process.env.OWNER_NAME || 'lazack28', true],
+  [process.env.OWNER_NUMBER || '255734980103', process.env.OWNER_NAME || 'Canyon ', true],
   ['255779679079', 'lazack 28', true]
 ]
 global.mods = []
