9c837c77b648d1dda7bc43c7b63b57cd7146233c
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-21 16:16:13 +0300

    Update Botav.js

--- lazackcmds/Botav.js
+++ lazackcmds/Botav.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn}) => {
 let name = conn.getName(m.sender)
 let av = `./jusorts/${pickRandom(["lazack", "mtaju"])}.mp3`
 
-conn.sendButton(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+/*conn.sendButton(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
       ['⦙☰ Menu', '.help'],
       ['⦙☰ Menu 2', '.menu2'],
       [`⌬ Support`, '.grp']
-    ], m) 
+    ], m) */
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
