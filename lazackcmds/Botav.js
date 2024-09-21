667e266107cfe35f3528d4bff34d88d00bf1dafc
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-21 16:22:46 +0300

    Update Botav.js

--- lazackcmds/Botav.js
+++ lazackcmds/Botav.js
@@ -1,19 +1,14 @@
+let handler = async (m, { conn }) => {
+  let user = global.db.data.users[m.sender]
+  let name = conn.getName(m.sender)
+  let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
+  let av = `./jusorts/${pickRandom(['lazack', 'mtaju'])}.mp3`
 
-let handler = async (m, { conn}) => {
-
-let name = conn.getName(m.sender)
-let av = `./jusorts/${pickRandom(["lazack", "mtaju"])}.mp3`
-
-/*conn.sendButton(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-      ['⦙☰ Menu', '.help'],
-      ['⦙☰ Menu 2', '.menu2'],
-      [`⌬ Support`, '.grp']
-    ], m) */
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
 handler.customPrefix = /^(bot|lazack)$/i
-handler.command = new RegExp
+handler.command = new RegExp()
 
 export default handler
 
