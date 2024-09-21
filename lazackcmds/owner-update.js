725cda526f1d520706b7faf911e4b8ddd0721764
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-22 01:56:07 +0300

    Update owner-update.js

--- lazackcmds/owner-update.js
+++ lazackcmds/owner-update.js
@@ -1,10 +1,10 @@
-//import db from '../lib/database.js'
+import db from '../lib/database.js'
 
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
   if (conn.user.jid == conn.user.jid) {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-    //require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+    require('fs').readdirSync('plugins').map(v=>global.reload('', v))
     conn.reply(m.chat, stdout.toString(), m)
   }
 }
