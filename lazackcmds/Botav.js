8746259ed55b84991a475a47f3bbe93383750aaf
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-21 22:49:22 +0300

    Update Botav.js

--- lazackcmds/Botav.js
+++ lazackcmds/Botav.js
@@ -4,6 +4,12 @@ let handler = async (m, { conn }) => {
   let taguser = '@' + m.sender.split('@s.whatsapp.net')[0]
   let av = `./jusorts/${pickRandom(['lazack', 'mtaju'])}.mp3`
 
+
+conn.sendButton(m.chat, `*HOLA FROM BOT LAZACK*      
+    morning or evening @${m.sender.split('@')[0]} 
+    You called me what is youre problem bro? 😇
+  `.trim(), igfg, null, [['OWNER HELP', '.grp'],['GET SC', '.repo']] , m, { mentions: [m.sender] })
+  
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
