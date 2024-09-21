00273bd4db4abba6dc2afc113d25eb6acb436a8b
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-22 00:22:45 +0300

    Update devotional-mahadev.js

--- lazackcmds/devotional-mahadev.js
+++ lazackcmds/devotional-mahadev.js
@@ -7,7 +7,10 @@ let endpoint = `https://shizoapi.onrender.com/api/devotional/mahadev?apikey=shiz
 const response = await fetch(endpoint);
 if (response.ok) {
       const imageBuffer = await response.buffer();
-      await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpig);
+      await conn.sendButton(m.chat, imageBuffer, 'shizo.techie.err.png', msg, m, null, rpig);
+  .trim(), igfg, null, [['NEXT', '.shiva'],['GET SC', '.repo']] , m, { mentions: [m.sender] })
+  
+     // await conn.sendFile(m.chat, imageBuffer, 'shizo.techie.error.png', msg, m, null, rpig);
     } else {
       throw bug
     }
