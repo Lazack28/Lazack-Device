520ddd3c18307a5fa7265bb978ae0865c8705ee0
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-23 09:19:06 +0300

    Update dl_play2.js

--- lazackcmds/dl_play2.js
+++ lazackcmds/dl_play2.js
@@ -2,11 +2,11 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
-  if (!text) throw `✳️*${usedPrefix + command}* Lil Peep hate my life`
+  if (!text) throw `✳️ *${usedPrefix + command}* Lil Peep hate my life`
 	let res = await yts(text)
 	let vid = res.videos[0]
 	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
-	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
+	let { title, description, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
 	m.react('🎧') 
   let play = `
