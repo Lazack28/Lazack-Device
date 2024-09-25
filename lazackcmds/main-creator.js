f0d5ca0e98d888b10243e45811ebd783161bd735
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-25 14:43:03 +0300

    Update main-creator.js

--- lazackcmds/main-creator.js
+++ lazackcmds/main-creator.js
@@ -1,14 +1,9 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(
-    m.chat,
-    data.map(([id, name]) => [id, name]),
-    m
-  )
+let handler = async (m, { conn, usedPrefix, isOwner }) => {
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;lazack;;\nFN:lazack⁩\nORG:lazack\nTITLE:\nitem1.TEL;waid=255734980103:255734980103\nitem1.X-ABLabel:lazack\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:lazack\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'LAZACK_28', contacts: [{ vcard }] }}, {quoted: m})
 }
-
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
 export default handler
