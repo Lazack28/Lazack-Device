0a8e75dab16715fc64c51302f32accd9d968d723
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-27 00:41:53 +0300

    Update dl-spotify.js

--- lazackcmds/dl-spotify.js
+++ lazackcmds/dl-spotify.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
     throw `*Please enter a song name*`
   }
   m.react('🎶')
-  await displayLoadingScreen(conn, m.chat)
+ // await displayLoadingScreen(conn, m.chat)
   let pp = 'https://wallpapercave.com/wp/wp7932387.jpg'
   const query = encodeURIComponent(text)
   let res = `https://guruapi.tech/api/spotifydl?url=${query}`
