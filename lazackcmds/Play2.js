2186ddc2d455db2500c2a06efbc638246ebb82e4
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-27 00:49:10 +0300

    Update Play2.js

--- lazackcmds/Play2.js
+++ lazackcmds/Play2.js
@@ -1,1 +1,42 @@
 
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text }) => {
+  if (!text) {
+    console.log('No song name provided.')
+    throw `*Please enter a song name*`
+  }
+  m.react('🎶')
+ // await displayLoadingScreen(conn, m.chat)
+  let pp = 'https://wallpapercave.com/wp/wp7932387.jpg'
+  const query = encodeURIComponent(text)
+  let res = `https://apisku-furina.vercel.app/api/downloader/play?q=${query}&apikey=indradev`
+  // let spotify = await (await fetch(res)).buffer()
+ /* let doc = {
+    audio: {
+      url: res,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'shizo.mp3',
+
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: '↺ |◁   II   ▷|   ♡',
+        body: `Now playing: ${text}`,
+        thumbnailUrl: pp,
+        sourceUrl: null,
+        mediaType: 1,
+        renderLargerThumbnail: false,
+      },
+    },*/
+  }
+
+  await conn.sendMessage(m.chat, { quoted: m })
+}
+handler.help = ['spotify']
+handler.tags = ['downloader']
+handler.command = /^(play1|song1)$/i
+
+export default handler
