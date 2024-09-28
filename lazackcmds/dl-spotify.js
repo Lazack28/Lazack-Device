be0a7b37e87867095d75b6ba8835e50ba6812d60
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-09-28 16:43:27 +0300

    Update dl-spotify.js

--- lazackcmds/dl-spotify.js
+++ lazackcmds/dl-spotify.js
@@ -1,43 +1,23 @@
+import Scraper from "@SumiFX/Scraper"
 
-import displayLoadingScreen from '../lib/loading.js'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text }) => {
-  if (!text) {
-    console.log('No song name provided.')
-    throw `*Please enter a song name*`
-  }
-  m.react('🎶')
-  await displayLoadingScreen(conn, m.chat)
-  let pp = 'https://wallpapercave.com/wp/wp7932387.jpg'
-  const query = encodeURIComponent(text)
-  let res = `https://apisku-furina.vercel.app/api/download/spotify?url=${query}&apikey=indradev`
-  // let spotify = await (await fetch(res)).buffer()
-  let doc = {
-    audio: {
-      url: res,
-    },
-    mimetype: 'audio/mpeg',
-    ptt: true,
-    waveform: [100, 0, 100, 0, 100, 0, 100],
-    fileName: 'shizo.mp3',
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('🍭 Ingresa el nombre de algún Track de Spotify.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`)
 
-    contextInfo: {
-      mentionedJid: [m.sender],
-      externalAdReply: {
-        title: '↺ |◁   II   ▷|   ♡',
-        body: `Now playing: ${text}`,
-        thumbnailUrl: pp,
-        sourceUrl: null,
-        mediaType: 1,
-        renderLargerThumbnail: false,
-      },
-    },
-  }
-
-  await conn.sendMessage(m.chat, doc, { quoted: m })
-}
-handler.help = ['spotify']
+let user = global.db.data.users[m.sender]
+try {
+let { title, artist, album, published, thumbnail, dl_url } = await Scraper.spotify(text)
+let txt = `╭─⬣「 *Spotify Download* 」⬣\n`
+    txt += `│  ≡◦ *🍭 Nombre ∙* ${title}\n`
+    txt += `│  ≡◦ *🪴 Artista ∙* ${artist}\n`
+    txt += `│  ≡◦ *📚 Album ∙* ${album}\n`
+    txt += `│  ≡◦ *📅 Publicado ∙* ${published}\n`
+    txt += `╰─⬣`
+await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
+await conn.sendFile(m.chat, dl_url, title + '.mp3', `*🍭 Titulo ∙* ${title}\n*🪴 Artista ∙* ${artist}`, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
+} catch {
+}}
+handler.help = ['spotify <búsqueda>']
 handler.tags = ['downloader']
-handler.command = /^(spotify|song)$/i
-
+handler.command = ['spotify']
+handler.register = true 
 export default handler
