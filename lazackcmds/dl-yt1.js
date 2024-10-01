021ff48a602c31fac057718681d045336c81c374
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-01 17:28:27 +0300

    Update dl-yt1.js

--- lazackcmds/dl-yt1.js
+++ lazackcmds/dl-yt1.js
@@ -1,4 +1,4 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import { youtubedl, youtubedlv2 } from '@youtubedl-core'
 
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
