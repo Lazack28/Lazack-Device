4130c44416fc61f2ecc09eb0c061a804db69ae9c
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-15 03:29:28 -0700

    mt

--- lazackcmds/tools-wikipedia.js
+++ lazackcmds/tools-wikipedia.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+import * cheerio from 'cheerio'
 
 let handler = async (m, { text }) => {
   if (!text) throw `✳️ Enter what you want to search for on Wikipedia`
