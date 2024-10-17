7ea98f9ff657398f189fe247daf3ebdebc470a81
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-17 12:38:10 +0300

    mtaju

--- lazackcmds/Hello.js
+++ lazackcmds/Hello.js
@@ -35,7 +35,7 @@ export async function all(m) {
       m.chat,
       `*WELCOME ITS LAZACK ASSISTANT BOT REPLYING HERE*\n\n      
     Hello 💕🥰 @${m.sender.split('@')[0]}\n\n 
-    Lazack organisation offers different things and you are kindly welcome.. the following are what we offer:-\n\n 1. Heroku card price 3000/ shillings\n 2. Bot deployment price 2500/- shillings\n 3. Halotel bundles, weekly and monthly bundles start price is 3000/- shillings\n 4. Social media boosting\n\n > offer only in Tanzania\n\n USE BUTTON TO FIND OWNER`.trim(),
+    Lazack organisation offers different things and you are kindly welcome.. the following are what we offer:-\n\n1. Heroku card price 3000/ shillings\n2. Bot deployment price 2500/- shillings\n3. Halotel bundles, weekly and monthly bundles start price is 3000/- shillings\n4. Social media boosting\n\n> offer only in Tanzania\n\nUSE BUTTON TO FIND OWNER`.trim(),
       igfg,
       null,
       [['GET OFFER', '.owner'], ['GROUPS', '.grp']],
