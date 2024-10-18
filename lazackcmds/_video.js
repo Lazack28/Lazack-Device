a47f58617b95ed2987fe0a9be43b9c90d168fc05
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-18 22:43:25 +0300

    mtaju

--- lazackcmds/_video.js
+++ lazackcmds/_video.js
@@ -1,25 +1,34 @@
 import Starlights from '@StarlightsTeam/Scraper'
 let limit = 300
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `🐯 Tag the message containing the YouTube Play result.`, m, rcanal).then(_ => m.react('✖️'))
-if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `🐯 Tag the message containing the YouTube Play result.`, m).then(_ => m.react('✖️'))
+if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m).then(_ => m.react('✖️'))
+if (!m.quoted.text.includes("乂  Y O U T U B E  -  P L A Y")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m).then(_ => m.react('✖️'))
 let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
-if (!urls) return conn.reply(m.chat, `Result not found.`, m).then(_ => m.react('✖️'))
-if (urls.length < text) return conn.reply(m.chat, `Result not found.`, m).then(_ => m.react('✖️'))
+if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m).then(_ => m.react('✖️'))
+if (urls.length < text) return conn.reply(m.chat, `Resultado no Encontrado.`, m).then(_ => m.react('✖️'))
 let user = global.db.data.users[m.sender]
 
 await m.react('🕓')
 try {
 let v = urls[0]
+let { title, duration, size, thumbnail, dl_url } = await Starlights.ytmp4v2(v)
+
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
+
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}`, m, false, { asDocument: user.useDocument })
+await m.react('✅')
+} catch {
+try {
+let v = urls[0]
 let { title, size, quality, thumbnail, dl_url } = await Starlights.ytmp4(v)
 
-if (size.split('MB')[0] >= limit) return m.reply(`The file weighs more than ${limit} MB, Download was canceled.`).then(_ => m.react('✖️'))
+if (size.split('MB')[0] >= limit) return m.reply(`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`).then(_ => m.react('✖️'))
 
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Title* : ${title}\n*» Quality* : ${quality}`, m, false, { asDocument: user.useDocument })
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}\n*» Calidad* : ${quality}`, m, false, { asDocument: user.useDocument })
 await m.react('✅')
 } catch {
 await m.react('✖️')
-}}
+}}}
 handler.help = ['Video']
 handler.tags = ['downloader']
 handler.customPrefix = /^(Video|video|vídeo|Vídeo)/
