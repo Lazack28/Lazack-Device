25031a492ee8500b377511fad239e41461135785
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-29 11:15:01 +0300

    MTAJU

--- lazackcmds/wall.js
+++ lazackcmds/wall.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
   //conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)
 }
-conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['img']
 handler.command = /^(ronaldo|cr7)$/i
