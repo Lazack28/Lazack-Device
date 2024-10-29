f83698a1dae25a041c0470dd4d1d55ffb11070e5
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-29 13:43:15 +0300

    mtaju

--- lazackcmds/dl-tiktok.js
+++ lazackcmds/dl-tiktok.js
@@ -1,103 +1,85 @@
-import axios from "axios";
-import cheerio from "cheerio";
-
-let handler = async (m, {text, usedPrefix, conn}) => {
-if (!text) return m.reply('${mssg.example}: .' + usedPrefix + ' past the link here...');
-try {
-m.reply(wait)
-let { media, status, isSlide, title } = await tiktok(text);
-if (status !== 200) throw 'error';
-let no = 1
-if (isSlide) {
-for (let res of media) {
-conn.sendFile(m.chat, res, '', `Image - ${no++}`, m)
-await conn.delay(500)
-}
-} else {
-conn.sendFile(m.chat, media.no_wm, '', `\`${mssg.title}: ${title}\``, m)
-}
-
-} catch (e) {
-throw e
-}
-}
-handler.help = ['tiktok','tk']
-handler.tags = ['downloader']
-handler.command = /^(tk2|tiktok2|tkk)$/i
-export default handler
-
-async function tiktok(urls) {
-
-    const url = 'https://tiktokio.com/api/v1/tk-htmx';
-  const { data: pref } = await axios.get('https://tiktokio.com/id/');
-const $$ = cheerio.load(pref);
-const prefixx = $$('input[name="prefix"]').attr('value');
-
-    const data = new URLSearchParams({
-        prefix: prefixx,
-        vid: urls
+const TikTok = async _0x2d5f72 => {
+  try {
+    const _0x12bd84 = await fetch("https://api.yanzbotz.live/api/downloader/tiktok?url=" + encodeURIComponent(_0x2d5f72) + "&apiKey=PrincelovesYanz");
+    const _0x2bb533 = await _0x12bd84.json();
+    return _0x2bb533.result;
+  } catch (_0x3c3043) {
+    console.error("Error", _0x3c3043);
+    throw _0x3c3043;
+  }
+};
+let handler = async (_0x21d369, {
+  conn: _0x4506e4,
+  args: _0x195d8c,
+  usedPrefix: _0x275bcf,
+  command: _0x319ecc
+}) => {
+  const _0x23ff37 = _0x195d8c[0];
+  if (!_0x23ff37) {
+    return _0x21d369.reply("*🟢Example*\n *.tiktok* past your link");
+  }
+  _0x21d369.react("⏳");
+  try {
+    let _0x463b35 = await TikTok(_0x23ff37);
+    let _0x5143fd = _0x463b35.type;
+    let _0x2c113b = "╭━━⊱𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗟 \n";
+    _0x2c113b += "🎗️ *" + mssg.type + ":* " + _0x5143fd + "\n";
+    _0x2c113b += "🎗️ *" + mssg.name + ":* " + _0x463b35.name + "\n";
+    _0x2c113b += "🎗️ *" + mssg.username + ":* " + _0x463b35.username + "\n";
+    _0x2c113b += "🎗️ *" + mssg.views + ":* " + _0x463b35.views + "\n";
+    _0x2c113b += "🎗️ *" + mssg.likes + ":* " + _0x463b35.likes + "\n";
+    _0x2c113b += "🎗️ *" + mssg.comments + ":* " + _0x463b35.comments + "\n";
+    _0x2c113b += "🎗️ *" + mssg.favorite + ":* " + _0x463b35.favorite + "\n";
+    _0x2c113b += "🎗️ *" + mssg.shares + ":* " + _0x463b35.shares + "\n";
+    _0x2c113b += "🎗️ *" + mssg.desc + ":* " + _0x463b35.description + "\n╰━━━━━━━━━━━━━━━━━";
+    _0x4506e4.sendMessage(_0x21d369.chat, {
+      "text": "📥 *" + mssg.media + ":* " + _0x5143fd
+    }, {
+      "quoted": _0x21d369
     });
-    const config = {
-        headers: {
-            'HX-Request': 'true',
-            'HX-Trigger': 'search-btn',
-            'HX-Target': 'tiktok-parse-result',
-            'HX-Current-URL': 'https://tiktokio.com/id/',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: data
-    };
-
-    try {
-
-        let {
-            data: res
-        } = await axios.post(url, data, config);
-        let $ = cheerio.load(res);
-        const urls = [];
-        let media;
-
-        const links = {
-            creator: 'tio',
-            status: 200,
-            isSlide: false,
-            title: $('h2').text(),
-            media: media
+    if (_0x5143fd === "image") {
+      let _0xf17107 = _0x463b35.image;
+      const _0x40a033 = {
+        "text": _0x2c113b
+      };
+      _0x4506e4.sendMessage(_0x21d369.chat, _0x40a033, {
+        "quoted": _0x21d369
+      });
+      for (let _0x22f40a = 0; _0x22f40a < _0xf17107.length; _0x22f40a++) {
+        const _0xb431c8 = {
+          url: _0xf17107[_0x22f40a]
         };
-
-        $('.download-item img').each((index, element) => {
-            const url = $(element).attr('src');
-            urls.push(url);
-            links.isSlide = true;
+        await _0x4506e4.sendMessage(_0x21d369.chat, {
+          "image": _0xb431c8,
+          "caption": "*🎗️image:* " + (_0x22f40a + 1)
+        }, {
+          "quoted": _0x21d369
         });
-
-        if (urls.length === 0) {
-            media = {};
-            $('div.tk-down-link').each(function(index, element) {
-                const linkType = $(this).find('a').text().trim();
-                const url = $(this).find('a').attr('href');
-
-                if (linkType === 'Download watermark') {
-                    media['watermark'] = url;
-                } else if (linkType === 'Download Mp3') {
-                    media['mp3'] = url;
-                } else if (linkType === 'Download without watermark') {
-                    media['no_wm'] = url;
-                } else if (linkType === 'Download without watermark (HD)') {
-                    media.hd = url;
-                }
-            });
-        } else {
-            media = urls;
-        }
-
-        links.media = media;
-
-        return links
-    } catch (e) {
-        return {
-            status: 404,
-            msg: e
-        }
+        const _0x289ae7 = {
+          "mimetype": "audio/mp4"
+        };
+        _0x4506e4.sendFile(_0x21d369.chat, _0x463b35.sound, "tiktok.mp3", '', _0x21d369, null, _0x289ae7);
+      }
     }
-}
+    if (_0x5143fd === "video") {
+      let _0x30fff4 = _0x463b35.video["no-watermark"];
+      const _0x5e160b = {
+        "url": _0x30fff4
+      };
+      const _0x253ad3 = {
+        "video": _0x5e160b,
+        "caption": _0x2c113b
+      };
+      await _0x4506e4.sendMessage(_0x21d369.chat, _0x253ad3, {
+        "quoted": _0x21d369
+      });
+    }
+  } catch (_0x4e3bf5) {
+    _0x21d369.reply('' + mssg.error);
+  }
+  _0x21d369.react("✅");
+};
+handler.help = ["tiktok"];
+handler.tags = ["tools"];
+handler.command = /^(tiktok|tt|tiktokdl|tiktokslide|tiktoknowm|tiktokvid|ttdl)$/i;
+export default handler;
