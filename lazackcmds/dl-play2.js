b11c467c59df72c976b136336d7bc08947b0bfe7
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-29 15:02:22 +0300

    MTAJU

--- lazackcmds/dl-play2.js
+++ lazackcmds/dl-play2.js
@@ -26,7 +26,7 @@ let handler = async (_0x5cc456, {
   _0x5cc456.react("🎧");
   _0x5cc456.react(wait);
   let _0x28b98e = "\n╭━━━⊱⛲*DEVICE PLAY*⛲⊱━━━╮\n┃📌 " + _0x2efa54.title + "\n┃📆 " + _0x2efa54.ago + "\n┃⌚  " + _0x2efa54.timestamp + "\n┃👀 " + _0x2efa54.views.toLocaleString() + "\n╰━━━━━━━━━━━━━━━━━━━━━━━━━╯";
-  await _0x2d3324.sendButton2(_0x5cc456.chat, _0x28b98e, "LAZACK MD ", _0x351f54, [["🎵 ᴍᴘ3", _0x359e44 + "song " + _0x30a24a], ["📼 ᴍᴘ4 ", _0x359e44 + "video " + _0x30a24a]], null, [["⭕ ᴄ ʜ ᴀ ɴ ɴ ᴇ ʟ", '' + princegp]], _0x5cc456);
+  await _0x2d3324.sendButton2(_0x5cc456.chat, _0x28b98e, "LAZACK MD ", _0x351f54, [["🎵 ᴍᴘ3", _0x359e44 + "song " + _0x30a24a], ["📼 ᴍᴘ4 ", _0x359e44 + "video " + _0x30a24a]], null, _0x5cc456);
 };
 handler.help = ["play"];
 handler.tags = ["dl"];
