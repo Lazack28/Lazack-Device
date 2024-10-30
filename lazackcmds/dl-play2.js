b4459b215fe0855deac037b96bc92ad3aae9faea
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-10-30 09:14:40 +0300

    Update dl-play2.js

--- lazackcmds/dl-play2.js
+++ lazackcmds/dl-play2.js
@@ -1,35 +1,132 @@
-import _0xa5c984 from "yt-search";
-let handler = async (_0x5cc456, {
-  conn: _0x2d3324,
-  command: _0x1ce9ee,
-  text: _0x30a24a,
-  usedPrefix: _0x359e44
-}) => {
-  if (!_0x30a24a) {
-    throw "✳️ *" + (_0x359e44 + _0x1ce9ee) + "* 𝙰𝚢𝚊𝚊 𝚑𝚊𝚒 𝚋𝚞𝚕𝚊𝚠𝚊 𝙽𝚊𝚊𝚝...";
+/*
+*[ Plugins Ytdl Audio Version]
+* Created By Cifumo
+* Channel: https://whatsapp.com/channel/0029VadvQ9K2UPB829z2tk1A
+* Manusia kan tau caranya menghargai bagaimana, kalo gatau berarti bukan manusia.
+*/
+
+
+
+
+import axios from "axios";
+
+
+const handler = async (m, { conn, args, command, usedPrefix }) => {
+  conn.youtube = conn.youtube || {};
+
+  if (!args[0])
+    return m.reply(
+      `Masukan Link Youtube!\n\nContoh :\n${usedPrefix + command} https://youtu.be/Wky7Gz_5CZs`
+    );
+  if (
+    !/https:\/\/www.youtube.com/i.test(args[0]) &&
+    !/https:\/\/youtube.com/i.test(args[0]) &&
+    !/https:\/\/youtu.be/i.test(args[0])
+  )
+    return m.reply("Masukkan URL YouTube yang benar");
+
+  const id = m.sender;
+  if (id in conn.youtube) return m.reply("Kamu masih mendownload!");
+
+  let ephemeral =
+    conn.chats[m.chat]?.metadata?.ephemeralDuration ||
+    conn.chats[m.chat]?.ephemeralDuration ||
+    false;
+
+  try {
+    conn.youtube[id] = true;
+    let setting = global.db.data.settings[conn.user.jid];
+
+      let req = await axios.get('https://rest.cifumo.biz.id/api/downloader/ytdl?url=' + args[0])
+      let dl = req.data.data
+      let title = dl.title || apalah.title
+      let { views, thumbnail, duration, description } = dl.metadata
+     let fileSizeInBytes = await Buffer.byteLength(await toBuffer(dl.video))
+
+   let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+
+    let text = "*[ Youtube Downloader ]*\n\n";
+    text += `╔╾┅━┅━┅━┅━┅━┅━┅━┅⋄\n`
+    text += `┇ ❏ \`Title:\` ${title}\n`;
+    text += `║ ❏ \`view:\` ${views || 'Null'}\n`;
+    text += `┇ ❏ \`Category:\` Audio (mp3)\n`;
+    if (duration) text += `║ ❏ \`Duration:\` ${duration}\n`;
+    text += `┇ ❏ \`File Size:\` ${fileSizeInMegabytes.toFixed(2)} MB\n`;
+    text += `╚╾┅━┅━┅━┅━┅━┅━┅━┅⋄`
+
+    const fChat = await conn.sendMessage(
+          m.chat,
+          {
+            text: text,
+            contextInfo: {
+              externalAdReply: {
+                showAdAttribution: false,
+                mediaType: 1,
+                title: title || 'none',
+                body: duration || "none",
+                thumbnailUrl: thumbnail,
+                renderLargerThumbnail: true,
+                mediaUrl: thumbnail,
+                sourceUrl: args[0],
+              },
+            },
+          },
+          { quoted: m },
+        );
+
+    const jpegThumbnail = await conn.resize(
+      thumbnail || apalah.thumbnail,
+      400,
+      400
+    );
+
+    if (fileSizeInMegabytes > 60) {
+      await conn.sendMessage(
+        m.chat,
+        {
+          document: await toBuffer(dl.video),
+          mimetype: "audio/mpeg",
+          fileName: `${title}.mp3`,
+          pageCount: 2024,
+          jpegThumbnail,
+          fileLength: fileSizeInBytes,
+        },
+        { quoted: fChat }
+      );
+    } else {
+      await conn.sendMessage(
+        m.chat,
+        {
+          audio: await toBuffer(dl.video),
+          fileName: `${title}.mp3`,
+          mimetype: "audio/mpeg",
+          caption: title,
+        },
+        { quoted: fChat, ephemeralExpiration: ephemeral }
+      );
+    }
+  } catch (e) {
+    throw e;
+  } finally {
+    delete conn.youtube[id];
   }
-  let _0x4f49ee = await _0xa5c984(_0x30a24a);
-  let _0x2efa54 = _0x4f49ee.videos[0];
-  if (!_0x2efa54) {
-    throw "✳️ Video/Audio not found";
-  }
-  let {
-    title: _0xad4e24,
-    description: _0x3f71df,
-    thumbnail: _0x351f54,
-    videoId: _0x39b0bf,
-    timestamp: _0x2b9870,
-    views: _0x21034f,
-    ago: _0x242d6e,
-    url: _0x38f915
-  } = _0x2efa54;
-  _0x5cc456.react("🎧");
-  _0x5cc456.react(wait);
-  let _0x28b98e = "\n *DEVICE PLAY* \n📌 " + _0x2efa54.title + "\n📆 " + _0x2efa54.ago + "\n⌚  " + _0x2efa54.timestamp + "\n👀 " + _0x2efa54.views.toLocaleString() + "\n";
-  await _0x2d3324.sendButton(_0x5cc456.chat, _0x28b98e, "Lazack Device ", _0x351f54, [["SONG", _0x359e44 + "song " + _0x30a24a], ["VID", _0x359e44 + "video " + _0x30a24a]], null, _0x5cc456);
 };
-handler.help = ["play"];
-handler.tags = ["dl"];
-handler.command = ["play"];
-handler.disabled = false;
+
+handler.help = ["ytmp4"];
+handler.tags = ["downloader"];
+handler.command = /^y((outube|tb)audio|(outube|tb?)mp3|utubaudio|taudio|ta)$/i;
+handler.limit = true;
+handler.error = 0;
 export default handler;
+
+async function toBuffer(url) {
+  try {
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+    });
+    return Buffer.from(response.data, "binary");
+  } catch (error) {
+    console.error("Terjadi kesalahan:", error);
+    throw error;
+  }
+  }
