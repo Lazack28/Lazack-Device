6b923bfa1cddc42221c668bbaa146592aa680486
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-11-10 20:00:53 +0300

    MTAJU

--- lazackcmds/lazack-Openai.js
+++ lazackcmds/lazack-Openai.js
@@ -26,21 +26,18 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
         throw new Error('No valid JSON response from the first API')
       }
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Guru322`]], m)
+      await conn.sendMessage(m.chat, { text: result }, { quoted: m })
       m.react(done)
     } catch (error) {
       console.error('Error from the first API:', error)
 
-      //const model = 'llama'
-     // const senderNumber = m.sender.replace(/[^0-9]/g, '')
-      //const session = `GURU_BOT_${senderNumber}`
       const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`
 
       let response = await fetch(guru2)
       let data = await response.json()
       let result = data.completion
 
-      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/2b96050ea3efcc108c228.jpg', [['Script', `.sc`]], null, [['Follow Me', `https://github.com/Lazack28`]], m)
+      await conn.sendMessage(m.chat, { text: result }, { quoted: m })
       m.react(done)
     }
   } catch (error) {
