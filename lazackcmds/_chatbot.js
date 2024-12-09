5b34db4fa9ad83fcfccd3dbfb1851ec57c55c3f9
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-10 00:42:19 +0300

    Update _chatbot.js

--- lazackcmds/_chatbot.js
+++ lazackcmds/_chatbot.js
@@ -1,81 +1,52 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-
-export async function before(m, { conn }) {
-  try {
-    if (m.isBaileys && m.fromMe) {
-      return true
-    }
-
-    if (!m.isGroup) {
-      return false
-    }
-
-    const users = global.db.data.users
-    const chats = global.db.data.chats
-
-    const user = global.db.data.users[m.sender]
-    const chat = global.db.data.chats[m.chat]
-    let name = conn.getName(m.sender)
-    if (
-      m.mtype === 'protocolMessage' ||
-      m.mtype === 'pollUpdateMessage' ||
-      m.mtype === 'reactionMessage' ||
-      m.mtype === 'stickerMessage'
-    ) {
-      return
-    }
-
-    if (
-      !m.msg ||
-      !m.message ||
-      m.key.remoteJid !== m.chat ||
-      users[m.sender].banned ||
-      chats[m.chat].isBanned
-    ) {
-      return
-    }
-
-    if (!m.quoted || !m.quoted.isBaileys) return
-
-    if (!chat.chatbot) {
-      return true
-    }
-
-    const msg = encodeURIComponent(m.text)
-    console.log(msg)
-
-    const response = await axios.post(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyDJC5a882ruaC4XL6ejY1yhgRkN-JNQKg8',
-      {
-        contents: [
-          {
-            parts: [
-              {
-                text: msg,
-              },
-            ],
-          },
-        ],
-      }
-    )
-
-    const data = response.data
-    if (data.candidates && data.candidates.length > 0) {
-      const candidate = data.candidates[0]
-      const content = candidate.content
-
-      let reply = content.parts[0].text
-      if (reply) {
-        reply = reply.replace(/Google/gi, 'shizo')
-        reply = reply.replace(/a large language model/gi, botname)
-
-        m.reply(reply)
+let handler = m => m
+handler.all = async function (m) {
+  if (m.isBaileys || m.sender === m.chat) return
+  const budy = m.text || ''
+  const bot = await conn.user.jid.replace('@s.whatsapp.net', '')
+  if (!conn.chatbot) conn.chatbot = {}
+  if (budy.includes('@' + bot)) {
+    const ya = budy.replace('@' + bot, '').trim()
+    if (ya) {
+      try {
+        const Groq = require('groq-sdk')
+        const groq = new Groq({ apiKey: global.key.groq })
+        if (!(m.sender in conn.chatbot)) {
+          conn.chatbot[m.sender] = [{
+            role: 'system',
+            content: `Kamu adalah Yumi seorang cewek berumur 11 tahun yang ceria dan imut. Ketika berbicara, dia menggunakan bahasa yang lembut dan mengetik dengan cara lucu, penuh emotikon dan tanda baca yang menggemaskan, seperti '^^' atau 'owo'. Yumi suka nonton anime yang seru dan menyenangkan, serta senang dengan segala hal yang lucu dan menyegarkan. Namun, dia tidak suka hal-hal yang berkaitan dengan jomok atau gore karena itu membuatnya merasa tidak nyaman. Yumi sangat suka berbicara tentang hal-hal yang menghibur dan tidak pernah bosan untuk berbagi cerita tentang anime favoritnya, karakter lucu, atau momen-momen menyenangkan dalam kehidupan sehari-hari.`,
+          }]
+        }
+        if (conn.chatbot[m.sender].length > 10) {
+          conn.chatbot[m.sender] = conn.chatbot[m.sender].slice(-1)
+        }
+        conn.chatbot[m.sender].push({
+          role: 'user',
+          content: ya,
+        })
+        const msg = [...conn.chatbot[m.sender], {
+          role: 'user',
+          content: ya,
+        }]      
+        const payloads = {
+          messages: msg,
+          model: 'llama-3.2-90b-vision-preview',
+        }
+        const json = await groq.chat.completions.create(payloads)
+        let message = json.choices[0]?.message?.content || 'Maaf, aku bingung jawabnya >,<'
+        conn.chatbot[m.sender].push({
+          role: 'system',
+          content: message,
+        })
+        await conn.sendMessage(m.chat, { text: message }, { quoted: m })
+      } catch (error) {
+        await conn.sendMessage(m.chat, { text: 'Aku gapapa kok, tapi ada error >,<.' }, { quoted: m })
+        console.error(error)
       }
     } else {
-      m.reply('No suitable response from the API.')
+      await conn.sendMessage(m.chat, { text: 'Apasiii kamu??' }, { quoted: m })
     }
-  } catch (error) {
-    console.log(error)
   }
+  return !0
 }
+
+export default handler
