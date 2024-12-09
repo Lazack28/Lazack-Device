d4eea98669986f34303287c5e393b750d2805239
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-09 18:58:47 +0300

    Create owner-report.js

---
+++ lazackcmds/owner-report.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+    if (!text) return conn.reply(m.chat, `Please enter your report\n\nExample: ${usedPrefix + command} Report user sending pornographic photos, please take action.`, m)
+
+    if (text.length > 300) return conn.reply(m.chat, 'Sorry, the text is too long. Maximum 300 characters.', m)
+
+    const teks1 = `*[ REPORT ]*\nNumber: wa.me/${m.sender.split("@s.whatsapp.net")[0]}\nMessage: ${text}`
+
+    conn.reply('255734980103@s.whatsapp.net', teks1, m)
+
+    conn.reply(m.chat, 'The issue has been successfully sent to the Owner', m)
+
+  }
+  handler.command = ['report']
+
+  export defult handler
