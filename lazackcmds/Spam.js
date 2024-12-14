f119be052a33cbad2d9faf0f63cb250e643f2ed1
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-14 04:57:43 +0300

    m

--- lazackcmds/Spam.js
+++ lazackcmds/Spam.js
@@ -1,18 +1,18 @@
 let handler = async (m, {conn, text}) => {
     const [nomor, pesan, jumlah] = text.split('|');
     
-    if (!nomor) return conn.reply(m.chat, '*Uso Correcto:*\n*🐉 #spamwa numero|texto|cantidad*', m, rcanal);
+    if (!nomor) return conn.reply(m.chat, '*Uso Correcto:*\n*🐉 #spamwa numero|texto|cantidad*', m,);
     
-    if (!pesan) return conn.reply(m.chat, '*Uso Correcto:*\n*🐉 #spamwa numero|texto|cantidad*', m, rcanal);
+    if (!pesan) return conn.reply(m.chat, '*Uso Correcto:*\n*🐉 #spamwa numero|texto|cantidad*', m,);
     
-    if (jumlah && isNaN(jumlah)) return conn.reply(m.chat, '*💨 La cantidad deve ser un numero*', m, rcanal);
+    if (jumlah && isNaN(jumlah)) return conn.reply(m.chat, '*💨 La cantidad deve ser un numero*', m,);
     
     const fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
     const fixedJumlah = jumlah ? jumlah * 1 : 10;
     
     if (fixedJumlah > 999) return conn.reply(m.chat, '*⚠️ Minimo 50 Caracteres*', m, fake);
     
-    await conn.reply(m.chat, '*☁️ Se envió con éxito el spam.*', m, rcanal);
+    await conn.reply(m.chat, '*☁️ Se envió con éxito el spam.*', m,);
     for (let i = fixedJumlah; i > 1; i--) {
     if (i !== 0) conn.reply(fixedNumber, pesan.trim(), null);
     }
