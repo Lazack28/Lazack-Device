f1e8ebab787fab81dae5b9d6baffc3311a497662
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-15 03:56:22 +0300

    mtaju

--- lazackcmds/love.js
+++ lazackcmds/love.js
@@ -1,3 +1,6 @@
+let regix = /love/i; // Regex pattern to match the word "love" (case insensitive)
+let asta = "I love you! 💖"; // Default message if the regex does not match
+
 let handler = async (m, { conn, args }) => {
     // Check if the input matches the regex; if not, use a default message
     let messageToSend = regix.test(args.join(' ')) ? args.join(' ') : asta;
