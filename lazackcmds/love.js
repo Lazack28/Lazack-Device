ee64cc9f8cd808a147ed191bcd0c8fda1f297e2c
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-15 03:51:53 +0300

    mtaju

---
+++ lazackcmds/love.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn, args }) => {
+    // Check if the input matches the regex; if not, use a default message
+    let messageToSend = regix.test(args.join(' ')) ? args.join(' ') : asta;
+
+    // Array of heart emojis
+    let heartEmojis = [
+        "❤", "💕", "😻", "🧡", "💛", "💚", "💙", "💜", "🖤", "❣", 
+        "💞", "💓", "💗", "💖", "💘", "💝", "💟", "♥", "💌", 
+        "🙂", "🤗", "😌", "😉", "🤗", "😊", "🎊", "🎉", "🎁", "❤"
+    ];
+
+    // Send the first heart emoji
+    let { key: messageKey } = await conn.reply(m.chat, messageToSend.replace(regix, "" + heartEmojis[0]), m);
+
+    // Loop through the heart emojis and send them with a delay
+    for (let i = 0; i < heartEmojis.length; i++) {
+        await sleep(800); // Wait for 800 milliseconds
+        await conn.reply(m.chat, messageToSend.replace(regix, "" + heartEmojis[i]), { edit: messageKey });
+    }
+}
+
+handler.help = ['love']
+handler.tags = ['fun']
+handler.command = ['love']
+// Change to true if only the owner should use it
+export default handler;
