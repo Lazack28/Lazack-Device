aa40fc2d087658bcb9bf4c4962caada31b266fd3
Lazack <164275612+Lazack28@users.noreply.github.com>
2024-12-18 16:21:44 +0300

    Update dl-yta.js

--- lazackcmds/dl-yta.js
+++ lazackcmds/dl-yta.js
@@ -1,72 +1,19 @@
-import fg from 'api-dylux'
-import yts from 'yt-search'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import fetch from 'node-fetch' 
-let limit = 100
 
-let handler = async (m, { conn: star, args, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!args || !args[0]) return star.reply(m.chat, '🐉 Enter the YouTube video link along with the command.\n\n`Example:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`, m, rcanal)
-if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `Verify that the link is from YouTube.`, m, rcanal).then(_ => m.react('✖️'))
-let q = '128kbps'
+// *[ ❀ YTMP3 ]*
+import fetch from 'node-fetch'
 
-await m.react('🕓')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `❀ please use ytube link`, m)
+    
 try {
-let v = args[0]
-let yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
-let dl_url = await yt.audio[q].download()
-let title = await yt.title
-let size = await yt.audio[q].fileSizeH
-let thumbnail = await yt.thumbnail
+let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
+let dl_url = api.data.dl
 
-let img = await (await fetch(`${thumbnail}`)).buffer()  
-if (size.split('MB')[0] >= limit) return star.reply(m.chat, `The file is larger than ${limit} MB, the download has been canceled.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 3`\n\n'
-       txt += `	📚   *Title* : ${title}\n`
-       txt += `	📹   *Quality* : ${q}\n`
-       txt += `	📃   *Size* : ${size}\n\n`
-       txt += `> *- 🚀 The audio is being sent, please wait a moment, I'm slow. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })
-await m.react('✅')
-} catch {
-try {
-let yt = await fg.yta(args[0], q)
-let { title, dl_url, size } = yt 
-let vid = (await yts(text)).all[0]
-let { thumbnail, url } = vid
+conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
 
-let img = await (await fetch(`${vid.thumbnail}`)).buffer()  
-if (size.split('MB')[0] >= limit) return star.reply(m.chat, `The file is larger than ${limit} MB, the download has been canceled.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 3`\n\n'
-       txt += `	✩   *Title* : ${title}\n`
-       txt += `	✩   *Quality* : ${q}\n`
-       txt += `	✩   *Size* : ${size}\n\n`
-       txt += `> *- ↻ The audio is being sent, please wait a moment, I'm slow. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })
-await m.react('✅')
-} catch {
-try {
-let yt = await fg.ytmp3(args[0], q)
-let { title, dl_url, size, thumb } = yt 
-
-let img = await (await fetch(`${thumb}`)).buffer()
-if (size.split('MB')[0] >= limit) return star.reply(m.chat, `The file is larger than ${limit} MB, the download has been canceled.`, m, rcanal).then(_ => m.react('✖️'))
-	let txt = '`乂  Y O U T U B E  -  M P 3`\n\n'
-       txt += `	✩   *Title* : ${title}\n`
-       txt += `	✩   *Quality* : ${q}\n`
-       txt += `	✩   *Size* : ${size}\n\n`
-       txt += `> *- ↻ The audio is being sent, please wait a moment, I'm slow. . .*`
-await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-await star.sendMessage(m.chat, { audio: { url: dl_url }, fileName: title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}}}
-handler.help = ['ytmp3 *<link yt>*']
-handler.tags = ['downloader']
-handler.command = ['ytmp3', 'yta', 'fgmp3']
-//handler.limit = 1
-handler.register = true 
+handler.command = ['ytmp3']
 
 export default handler
