410742c6e8bb9d557c9bf496800fc743e4ab57b3
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-01-02 21:13:00 +0300

    MIRCUS

--- lazackcmds/test2.js
+++ lazackcmds/test2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     let txt = `Nose?`
-    conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
+    conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: "MIRCUS", buttons: [
       {
         buttonId: ".menu", 
         buttonText: { 
