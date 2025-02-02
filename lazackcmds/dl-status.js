6a12d41d892fe61392b55fa6766540fc1b0dc239
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-02-03 02:55:36 +0300

    mtaju

--- lazackcmds/dl-status.js
+++ lazackcmds/dl-status.js
@@ -1,4 +1,4 @@
-const handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     // Only proceed if the command pattern is matched
     if (!command) return;
   
