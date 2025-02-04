6f151c5bbe3d4f832921d9b6e5a7a1cfc701a472
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-02-04 07:56:40 +0300

    Update dl-yta.js

--- lazackcmds/dl-yta.js
+++ lazackcmds/dl-yta.js
@@ -1,19 +1,20 @@
 
-// *[ ❀ YTMP3 ]*
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ please use ytube link`, m)
-    
+let handler = async (m, { conn, text }) => {
+if (!text) throw '• YouTube link is required.'
 try {
-let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${text}`)).json()
-let dl_url = api.data.dl
-
-conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", ptt: true }, { quoted: m })
-} catch (error) {
-console.error(error)
+let res = await fetch(`https://api.diioffc.web.id/api/download/ytmp3?url=${encodeURIComponent(text)}`)
+let json = await res.json()
+if (json.status && json.result?.download?.url) {
+let { title, thumbnail, views, duration, author, download } = json.result
+let caption = `• *Title:* ${title}\n• *Channel:* ${author.name}\n• *Duration:* ${duration.timestamp}\n• *Views:* ${views.toLocaleString()}`
+await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption }, { quoted: m })
+await conn.sendMessage(m.chat, { audio: { url: download.url }, mimetype: 'audio/mpeg', fileName: download.filename || 'audio.mp3' }, { quoted: m })
+} else throw 'No response to audio.'
+} catch (e) {
+m.reply(`❌ *Error:* Occurred during downloading audio`)
 }}
-
 handler.command = ['ytmp3']
 
 export default handler
