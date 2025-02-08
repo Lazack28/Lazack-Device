7998ab61469d6402312d0b24843ac193217d6e98
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-02-08 23:22:21 +0300

    Update _antilink.js

--- lazackcmds/_antilink.js
+++ lazackcmds/_antilink.js
@@ -1,33 +1,32 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-let linkRegex1 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
-
-export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, participants }) {
-if (!m.isGroup) return 
-if (isAdmin || isOwner || m.fromMe || isROwner) return
-
-let chat = global.db.data.chats[m.chat];
-let delet = m.key.participant;
-let bang = m.key.id;
-const user = `@${m.sender.split`@`[0]}`;
+let linkRegex1 = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
+let linkRegex2 = /whatsapp.com\/channel\/([0-9A-Za-z]{20,24})/i;
+export async function before(m, { isAdmin, isBotAdmin, participants }) {
+if (m.isBaileys && m.fromMe)
+return !0
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+let delet = m.key.participant
+let bang = m.key.id
+let toUser = `${m.sender.split("@")[0]}`
+let aa = toUser + '@s.whatsapp.net'
 const groupAdmins = participants.filter(p => p.admin);
 const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n');
-let bot = global.db.data.settings[this.user.jid] || {};
-const isGroupLink = linkRegex.exec(m.text) || linkRegex1.exec(m.text);
-const grupo = `https://chat.whatsapp.com`;
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('🏷 *Hey!! the anti-link is active but you are an admin, saved corrupt admin 🗣️‼️!*.');
+let bot = global.db.data.settings[this.user.jid] || {}
+const isGroupLink = linkRegex1.exec(m.text) || linkRegex2.exec(m.text);
+const grupo = `https://chat.whatsapp.com`
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(`${lenguajeGB['smsAdwa']()}`)
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-if (m.text.includes(linkThisGroup)) return !0;
-}
-await conn.sendMessage(m.chat, { text: `*「 📎 *Link Detected!* 」*\n\n${user} *!!Hey!! you sent a forbidden link, so you will be punished by all the group members*...`, mentions: [m.sender] }, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100 });
-if (!isBotAdmin) return conn.sendMessage(m.chat, { text: ` 🙄 *Dude, I'm not an admin, I can't remove intruders*.`, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
+const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+if (m.text.includes(linkThisGroup)) return m.reply(lenguajeGB['smsWaMismoEnlace']())  
+}    
+if (!isBotAdmin) return conn.sendMessage(m.chat, { text: `${lenguajeGB['smsAvisoFG']()} ${lenguajeGB['smsAllAdmin']()}`, mentions: [...groupAdmins.map(v => v.id)] }, { quoted: m });
 if (isBotAdmin) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
-let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-if (responseb[0].status === "404") return;
-}} // else if (!bot.restrict) {
-// return m.reply('🐉 This feature is disabled!');
-// }
-return !0;
+await conn.reply(m.chat, `${lenguajeGB['smsEnlaceWat']()} *@${toUser}*`, null, { mentions: [aa] })
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} else if (!bot.restrict) return m.reply(`${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`)
 }
+return !0
+                             }
