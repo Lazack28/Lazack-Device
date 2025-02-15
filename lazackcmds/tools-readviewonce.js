9800f3a6cf9298f6108c3f900f03bbb139a1338f
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-02-16 02:50:59 +0300

    mtaju

--- lazackcmds/tools-readviewonce.js
+++ lazackcmds/tools-readviewonce.js
@@ -1,15 +1,30 @@
-let { downloadContentFromMessage } = await import('@whiskeysockets/baileys')
+import { downloadContentFromMessage } from '@whiskeysockets/baileys';
 
 var handler = async (m, { conn }) => {
-  if (!/viewOnce/.test(m.quoted?.mtype)) throw '✳️❇️ Its Not a ViewOnce Message'
-  let mtype = Object.keys(m.quoted.message)[0]
-  let buffer = await m.quoted.download()
-  let caption = m.quoted.message[mtype].caption || ''
-  conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m })
-}
+  try {
+    if (!m.quoted || !/viewOnce/.test(m.quoted.mtype)) {
+      throw `✳️❇️ It's not a ViewOnce message.`;
+    }
+    
+    let mtype = Object.keys(m.quoted.message)[0];
+    let stream = await downloadContentFromMessage(m.quoted.message[mtype], mtype.replace('Message', ''));
+    
+    let buffer = Buffer.from([]);
+    for await (const chunk of stream) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+    
+    let caption = m.quoted.message[mtype]?.caption || '';
+    
+    await conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    throw '❌ An error occurred while processing the ViewOnce message.';
+  }
+};
 
-handler.help = ['readvo']
-handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'vv', 'readvo']
+handler.help = ['readvo'];
+handler.tags = ['tools'];
+handler.command = ['readviewonce', 'read', 'vv', 'readvo'];
 
-export default handler
+export default handler;
