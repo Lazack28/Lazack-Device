4915026feaeba26ea4ee531150373a1dc960e1b4
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-03-20 10:45:29 +0300

    mtaju

--- startup/config.js
+++ startup/config.js
@@ -84,7 +84,7 @@ global.fgsc = 'https://github.com/Lazack28/Lazack-Device'
 global.fgyt = 'https://youtube.com/@lazaromtaju'
 global.fgpyp = 'https://youtube.com/@lazaromtaju'
 global.fglog = ''
-global.thumb = fs.readFileSync('./img/lazack.jpg')
+global.thumb = fs.readFileSync(path.join(__dirname, 'img', 'lazack.jpg'));
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
