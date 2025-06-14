9243988ea6d7d964be3a85a0110fcba37dadf8e3
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-06-14 13:21:52 +0300

    mtaju

--- startup/LazackFlow.js
+++ startup/LazackFlow.js
@@ -38,7 +38,8 @@ import yargs from 'yargs'
 import CloudDBAdapter from '../lib/cloudDBAdapter.js'
 import { MongoDB } from '../lib/mongoDB.js'
 import { makeWASocket, protoType, serialize } from '../lib/simple.js'
-import { makeInMemoryStore } from '@whiskeysockets/baileys'
+const { makeInMemoryStore } = require('@whiskeysockets/baileys')
+
 
 
 const {
