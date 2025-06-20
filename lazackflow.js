95881170c8d6a3a74383c3b47e6e5e97efa03476
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-06-20 12:46:04 +0300

    MTAJU

--- lazackflow.js
+++ lazackflow.js
@@ -205,6 +205,7 @@ if (!conn.authState.creds.registered) {
       process.send({ 
         type: 'pairing-code', 
         code: 'ERROR: No phone number provided', 
+        desc: 'MADE BY LAZACK-TEAM',
         error: true 
       })
     }
