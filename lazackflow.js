c9bb1b2bc5ff3702a06648e0467c904c0652fd63
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-06-20 12:49:12 +0300

    MTAJU

--- lazackflow.js
+++ lazackflow.js
@@ -213,7 +213,7 @@ if (!conn.authState.creds.registered) {
 
   setTimeout(async () => {
     try {
-      let code = await conn.requestPairingCode(phoneNumber, "LAZACK28")
+      let code = await conn.requestPairingCode(phoneNumber, "LAZACKDV")
       code = code?.match(/.{1,4}/g)?.join('-') || code
       
       global.pairingCode = code
