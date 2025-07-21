34c967d7013b6c9e911d24e1e6f9140a2abc461e
Lazack <164275612+Lazack28@users.noreply.github.com>
2025-07-21 08:27:07 +0300

    Update main-script.js

--- lazackcmds/main-script.js
+++ lazackcmds/main-script.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn }) => {
     txt += `✩  *Name* : ${json.name}\n`
     txt += `✩  *Watchers* : ${json.watchers_count}\n`
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `✩  *Updated* : ${moment(json.updated_at).tz('America/Lima').format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `✩  *Updated* : ${moment(json.updated_at).tz('Africa/Nairobi').format('DD/MM/YY - HH:mm:ss')}\n`
     txt += `✩  *Url* : ${json.html_url}\n`
     txt += `✩  *Forks* : ${json.forks_count}\n`
     txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
